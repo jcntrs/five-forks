8e9f27c6425a9b72b507d3cb8fa3821729ca70d3
Jean C. Contreras <jean.cntrs@gmail.com>
2020-02-01 01:34:53 -0300

    Installing react native elements, navigation and creating the main views

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Icon } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Five Forks</Text>
+      <Button
+        title="Button with icon component"
+        loading={false}
+        icon={<Icon type="material-community" name="check-outline" size={15} color="white" />}
+      />
     </View>
   );
 }
