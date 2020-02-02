6c48bab591d3d93b870815f6456b32392d91f921
Jean C. Contreras <jean.cntrs@gmail.com>
2020-02-01 21:06:11 -0300

    Creating loading component

--- app/screens/Account/MyAccount.js
+++ app/screens/Account/MyAccount.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import * as firebase from 'firebase';
+import Loading from '../../components/Loading';
 
 const MyAccount = () => {
 
@@ -13,11 +14,7 @@ const MyAccount = () => {
     }, []);
 
     if (isLogged === null) {
-        return (
-            <View>
-                <Text>Cargando...</Text>
-            </View>
-        );
+        return <Loading isVisible={true} text="cargando..." />;
     }
 
     if (isLogged) {
