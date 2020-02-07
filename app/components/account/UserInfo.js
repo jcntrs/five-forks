b2e6348a13a6c21e1197032eaa857a443eb01d1d
jean.cntrs <j.contreras.open@gmail.com>
2020-02-07 18:03:46 -0300

    Showing user data in user info component

--- app/components/account/UserInfo.js
+++ app/components/account/UserInfo.js
@@ -1,16 +1,57 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Avatar } from 'react-native-elements';
+import * as firebase from 'firebase';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
-const UserInfo = ({ userInfo }) => {
-    if (Object.keys(userInfo).length) {
-        console.log(userInfo);
+const styles = StyleSheet.create({
+    viewUserInfo: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#f2f2f2',
+        paddingTop: 30,
+        paddingBottom: 30
+    },
+    avatarUserInfo: {
+        marginRight: 20
+    },
+    displayName: {
+        fontWeight: 'bold'
+    },
+    viewCenter: {
+        alignItems: 'center'
     }
+});
+
+const UserInfo = props => {
+
+    const { userInfo: { uid, displayName, email, photoURL } } = props;
+
+    const editAvatar = async () => {
+        const resultPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    }
+
     return (
-        <View>
-            <Text>UserInfo...</Text>
+        <View style={styles.viewUserInfo}>
+            <Avatar
+                rounded
+                size="large"
+                showEditButton
+                onEditPress={editAvatar}
+                containerStyle={styles.avatarUserInfo}
+                source={{ uri: photoURL ? photoURL : 'https://api.adorable.io/avatars/266/abott@adorable.png' }}
+            />
+            <View style={styles.viewCenter}>
+                <Text style={styles.displayName}>
+                    {displayName ? displayName : 'Anónimo'}
+                </Text>
+                <Text>{email ? email : 'Sesión Facebook'}</Text>
+            </View>
         </View>
     );
+
 }
 
 export default UserInfo;
