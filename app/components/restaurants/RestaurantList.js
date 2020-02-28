f2510b393dc4de74649b2f5c313bdd689a28ba67
Jean C. Contreras <jean.cntrs@gmail.com>
2020-02-28 01:19:46 -0300

    Creating component to list restaurants part 2

--- app/components/restaurants/RestaurantList.js
+++ app/components/restaurants/RestaurantList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { Image } from 'react-native-elements';
 import * as firebase from 'firebase';
@@ -7,17 +7,61 @@ const styles = StyleSheet.create({
     loadingRestaurants: {
         marginTop: 20,
         alignItems: 'center'
+    },
+    viewRestaurant: {
+        flexDirection: 'row',
+        margin: 10
+    },
+    viewRestaurantImage: {
+        marginRight: 15
+    },
+    imageRestaurant: {
+        width: 80,
+        height: 90
+    },
+    restaurantName: {
+        fontWeight: 'bold'
+    },
+    restaurantAddress: {
+        paddingTop: 2,
+        color: 'grey'
+    },
+    restaurantDescription: {
+        paddingTop: 2,
+        color: 'grey',
+        width: 300
     }
 });
 
 const Restaurant = props => {
 
     const { name, address, description, images } = props.restaurant.item.restaurant;
-    console.log(name)
+    const [imageURI, setImageURI] = useState(null);
+
+    useEffect(() => {
+        const imageName = images[0];
+        firebase.storage().ref(`restaurant-images/${imageName}`).getDownloadURL().then(result => {
+            setImageURI(result);
+        });
+    });
 
     return (
         <TouchableOpacity onPress={() => console.log('Ir al restaurante')}>
-            <View></View>
+            <View style={styles.viewRestaurant}>
+                <View style={styles.viewRestaurantImage}>
+                    <Image
+                        source={{ uri: imageURI }}
+                        style={styles.imageRestaurant}
+                        resizeMode="cover"
+                        PlaceholderContent={<ActivityIndicator color="#fff" />}
+                    />
+                </View>
+                <View>
+                    <Text style={styles.restaurantName}>{name}</Text>
+                    <Text style={styles.restaurantAddress}>{address}</Text>
+                    <Text style={styles.restaurantDescription}>{address.substr(0, 60)}...</Text>
+                </View>
+            </View>
         </TouchableOpacity>
     );
 
